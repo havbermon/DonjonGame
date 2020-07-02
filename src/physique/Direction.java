36e87c11868c4519ee3efea409a1224bbbff43d8
Luka73 <luanafernandes73@gmail.com>
2020-07-01 21:18:41 -0400

    add methods on Direction

--- src/physique/Direction.java
+++ src/physique/Direction.java
@@ -7,40 +7,47 @@ public class Direction {
     public final int DROITE = 3;
     public int random;
 
-    public void directionOpposee() {
-
+    public int directionOpposee(int direction) {
+        switch (direction) {
+            case HAUT :
+                return BAS;
+            case BAS :
+                return HAUT;
+            case GAUCHE :
+                return DROITE;
+            case DROITE :
+                return GAUCHE;
+            default:
+                return -1;
+        }
     }
 
     public Position directionAPosition(int direction) {
-        Position pos = new Position(0, 0);
         switch (direction) {
-            case 0 :
-                pos.setI(-1);
-                pos.setJ(0);
-                break;
-            case 1 :
-                pos.setI(1);
-                pos.setJ(0);
-                break;
-            case 2 :
-                pos.setI(0);
-                pos.setJ(-1);
-                break;
-            case 3 :
-                pos.setI(0);
-                pos.setJ(1);
-                break;
+            case HAUT :
+                return new Position(-1, 0);
+            case BAS :
+                return new Position(1, 0);
+            case GAUCHE :
+                return new Position(0, -1);
+            case DROITE :
+                return new Position(0, 1);
+            default:
+                return new Position(0, 0);
         }
-        return pos;
     }
 
     public int positionADirection(Position pos) {
 
-        if(pos.getI() == -1 && pos.getJ() == 0)  return 0;
-        if(pos.getI() == 1 && pos.getJ() == 0) return 1;
-        if(pos.getI() == 0 && pos.getJ() == -1) return 2;
-        if(pos.getI() == 0 && pos.getJ() == 1) return 3;
+        if(pos.getI() == -1 && pos.getJ() == 0)  return HAUT;
+        if(pos.getI() == 1 && pos.getJ() == 0) return BAS;
+        if(pos.getI() == 0 && pos.getJ() == -1) return GAUCHE;
+        if(pos.getI() == 0 && pos.getJ() == 1) return DROITE;
 
+        return -1;
+    }
+
+    public int obtenirDirAlea() {
         return -1;
     }
 }
